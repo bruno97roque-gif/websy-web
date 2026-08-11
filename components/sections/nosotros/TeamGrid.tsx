4af76ca0be05c5eb0091b4ffb7d580951c71c9f6
Luis <luisdiestrasolis@gmail.com>
2026-08-11 18:29:50 -0500

    feat(equipo): actualiza foto de Rubid Camacho

--- components/sections/nosotros/TeamGrid.tsx
+++ components/sections/nosotros/TeamGrid.tsx
@@ -121,7 +121,7 @@ const TEAM = [
   // Ingenieros
   { name: "Luis",          surname: "Diestra",    role: "Developer Full Stack",      tag: "Tecnología", Planet: RocketSVG, img: "/images/luis-developer.webp"    },
   { name: "Aaron",         surname: "Jauregui",   role: "Developer Full Stack",      tag: "Tecnología", Planet: RocketSVG, img: "/images/Aaron.webp"             },
-  { name: "Rubid",         surname: "Camacho",    role: "Ingeniera de Sistemas",     tag: "Tecnología", Planet: NebulaSVG, img: "/images/Rubid.png"             },
+  { name: "Rubid",         surname: "Camacho",    role: "Ingeniera de Sistemas",     tag: "Tecnología", Planet: NebulaSVG, img: "/images/Rubid-esta-si-es.png"             },
   // Diseñadores
   { name: "Ada",           surname: "",           role: "Brand Designer",            tag: "Diseño",     Planet: NebulaSVG, img: "/images/ada-diseñadora.webp"    },
   { name: "Juan Carlos",   surname: "Huapaya",    role: "Brand Designer",            tag: "Diseño",     Planet: NebulaSVG, img: "/images/Juancarlos.webp"        },
